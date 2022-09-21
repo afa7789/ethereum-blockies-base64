4c6805f200f6dad872c199babea8f0854e298f31
Arthur Abeilice <afa7789@gmail.com>
2022-09-21 11:26:22 -0300

    not really the best, but ok for now

--- src/main.js
+++ src/main.js
@@ -27,52 +27,12 @@ function rand() {
 function createColor() {
   //saturation is the whole color spectrum
   const h = Math.floor(rand() * 360);
-  //saturation goes from 40 to 100, it avoids greyish colors
-  const s = rand() * 60 + 40;
+  //saturation goes from 50 to 100, it avoids greyish colors
+  const s = rand() * 50 + 50;
   //lightness can be anything from 0 to 100, but probabilities are a bell curve around 50%
   const l = (rand() + rand() + rand() + rand()) * 25 ;
 
   return [h / 360, s / 100, l / 100];
-}
-
-function randomAngle(){
-  // it goes from -360 to 360
-  return rand() * 720 - 360
-}
-
-function createImageData(size) {
-  const width = size; // Only support square icons for now
-  const height = size;
-
-  const dataWidth = Math.ceil(width / 2);
-  const mirrorWidth = width - dataWidth;
-
-  const data = [];
-  for (let y = 0; y < height; y++) {
-    let row = [];
-    for (let x = 0; x < dataWidth; x++) {
-      // this makes foreground and background color to have a 43% (1/2.3) probability
-      // spot color has 13% chance
-      row[x] = Math.floor(rand() * 2.3);
-    }
-    const r = row.slice(0, mirrorWidth).reverse();
-    row = row.concat(r);
-
-    for (let i = 0; i < row.length; i++) {
-      data.push(row[i]);
-    }
-  }
-
-  return data;
-}
-
-// Modifies the passed PNG to fill in a specified rectangle
-function fillRect(png, x, y, w, h, color) {
-  for(let i = 0; i < w; i++) {
-    for (let j = 0; j < h; j++) {
-      png.buffer[png.index(x + i, y + j)] = color;
-    }
-  }
 }
 
 function buildOpts(opts) {
@@ -159,34 +119,18 @@ function makeGradient(address) {
   const color2 = `rgb(${spotcolor[0]},${spotcolor[1]},${spotcolor[2]})`
   const color3 = `rgb(${extracolor[0]},${extracolor[1]},${extracolor[2]})`
 
-  random1_X1 = randomAngle()
-  random1_X2 = randomAngle()
-  random2_X1 = randomAngle()
-  random2_X2 = randomAngle()
-
-  random1_Y1 = randomAngle()
-  random1_Y2 = randomAngle()
-  random2_Y1 = randomAngle()
-  random2_Y2 = randomAngle()
-
   let SvgStatic = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 ${calculated} ${calculated}">
     <defs>
-      <linearGradient id="b">
-        <stop offset="0" stop-color="${color1}"/>
-        <stop offset="1" stop-color="${color2}"/>
-      </linearGradient>
       <linearGradient id="a">
         <stop offset="0" stop-color="${color3}"/>
         <stop offset="1" stop-opacity="0"/>
       </linearGradient>
-      <linearGradient xlink:href="#a" id="d" 
-        x1="${random1_X1}" x2="${random1_X2}" y1="${random1_Y1}" y2="${random1_Y1}" 
-        gradientTransform="translate(.2 26.325)" gradientUnits="userSpaceOnUse"
-      />
-      <linearGradient xlink:href="#b" id="c" 
-        x1="${random2_X1}" x2="${random2_X2}" y1="${random2_Y1}" y2="${random2_Y1}"
-        gradientTransform="translate(.2 26.325)" gradientUnits="userSpaceOnUse"
-      />
+      <linearGradient id="b">
+        <stop offset="0" stop-color="${color1}"/>
+        <stop offset="1" stop-color="${color2}"/>
+      </linearGradient>
+      <linearGradient xlink:href="#a" id="d" x1="45.214" x2="118.616" y1="16.659" y2="198.682" gradientTransform="translate(.2 26.325)" gradientUnits="userSpaceOnUse"/>
+      <linearGradient xlink:href="#b" id="c" x1="-18.396" x2="287.531" y1="153.501" y2="84.825" gradientTransform="translate(.2 26.325)" gradientUnits="userSpaceOnUse"/>
     </defs>
     <g paint-order="fill markers stroke">
       <path fill="url(#c)" d="M10.027 63.278h185.208v185.208H10.027z" transform="translate(-10.027 -63.278)"/>
